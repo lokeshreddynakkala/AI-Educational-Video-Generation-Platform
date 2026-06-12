6f19cce4124c135f879a74f1fe7c7419d9d05ab9
N Lokesh Reddy <lokesh67b2@gmail.com>
2026-06-12 17:13:14 +0530

    Delete routers directory

--- routers/__init__.py
+++
@@ -1,8 +0,0 @@
-# Router modules
-from . import script
-from . import slides
-from . import voice
-from . import video
-from . import subtitle
-
-__all__ = ["script", "slides", "voice", "video", "subtitle"]
